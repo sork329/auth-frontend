4391c1e046212113349df307aebf0174d4eb8889
sork329 <adlmlv7@gmail.com>
2025-04-05 09:33:37 +0300

    fix global css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // добавляем autoprefixer
   },
 };
-
-export default config;
